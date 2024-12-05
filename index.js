612885a7f60c6a1e18b0cd4dc131ed90707aa284
Rinidh <rinidhdhokia@gmail.com>
2024-12-05 16:07:28 +0300

    Click number input buttons to display big numbers.. Display the dot between numbers but not twice in a number

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const buttons = document.querySelectorAll("button")
 const screen = document.getElementsByClassName("screen")[0]
 
 buttons.forEach(button => {
+  //actions for buttons having number inputs
   if (button.id && button.id !== "button-dot" && button.id !== "button-clear") {
     button.addEventListener("click", e => {
-      screen.innerText = e.target.innerText;
+      screen.innerText += e.target.innerText;
+    })
+  }
+
+  //actions for dot button
+  if (button.id === "button-dot") {
+    button.addEventListener("click", e => {
+      if (!screen.innerText.includes(".")) {
+        screen.innerText += e.target.innerText;
+      }
     })
   }
 })
