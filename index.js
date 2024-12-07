0d56b5746c612ff892c35ce7d6d357e288aff13d
Rinidh <rinidhdhokia@gmail.com>
2024-12-07 12:17:18 +0300

    For each button click, Identify the type of button

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const calculator = document.querySelector(".calculator")
+const keysDiv = document.querySelector(".calculator__keys")
+const display = document.querySelector('.calculator__display')
+
+keysDiv.addEventListener("click", e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+
+    //when the button was a digit
+    if (!action) {
+      console.log("a number");
+    }
+    //when the button was +, -, * or /
+    if (action === "add" || action === "multiply" || action === "subtract" || action === "divide") {
+      console.log("an operator")
+    }
+    if (action === "decimal") {
+      console.log('a decimal');
+    }
+    if (action === 'clear') {
+      console.log('ac button');
+
+    }
+    if (action === 'calculate') {
+      console.log('equals button');
+
+    }
+  }
+})
+
 
