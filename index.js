f054550b899e0f533fec9c304d8dc59e0080910a
Rinidh <rinidhdhokia@gmail.com>
2024-12-07 12:41:00 +0300

    Display numbers & decimal point on screen upon clicking

--- index.js
+++ index.js
@@ -4,19 +4,25 @@ const display = document.querySelector('.calculator__display')
 
 keysDiv.addEventListener("click", e => {
   if (e.target.matches('button')) {
+    //the code in this block re-runs on each button-click
+
     const key = e.target
     const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
 
-    //when the button was a digit
     if (!action) {
-      console.log("a number");
+      if (displayedNum === '0') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent //not addition but concatenation in strings
+      }
     }
-    //when the button was +, -, * or /
     if (action === "add" || action === "multiply" || action === "subtract" || action === "divide") {
       console.log("an operator")
     }
     if (action === "decimal") {
-      console.log('a decimal');
+      display.textContent = displayedNum + '.'
     }
     if (action === 'clear') {
       console.log('ac button');
