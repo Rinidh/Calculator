5f43a8357bd2231a6f5016351108dd04f5f45ac7
Rinidh <rinidhdhokia@gmail.com>
2024-12-12 12:50:02 +0300

    Refactor: Implement createResultString()

--- index.js
+++ index.js
@@ -4,62 +4,62 @@ const display = document.querySelector('.calculator__display')
 const clearButton = document.querySelector('[data-action=clear]') //access elements holding particular dataset
 
 keysDiv.addEventListener("click", e => {
-  if (e.target.matches('button')) {
-    //the code in this block re-runs on each button-click
+  if (!e.target.matches('button')) return; //if target was not a button, don't do anything
 
-    const key = e.target
-    const action = key.dataset.action
-    const keyContent = key.textContent
-    const displayedNum = display.textContent
-    const previousKeyType = calculator.dataset.previousKeyType;
-    // console.log(previousKeyType);
+  //all code below re-runs on each button-click
 
-    if (!action) {
-      calculator.dataset.previousKeyType = 'number' //to reset the custom dataset because now the last key pressed is not an operator key 
-    }
+  const key = e.target
+  const action = key.dataset.action
+  const displayedNum = display.textContent
 
-    if (action === "add" ||
-      action === "multiply" ||
-      action === "subtract" ||
-      action === "divide"
-    ) {
-      key.classList.add("is-depressed");
-      calculator.dataset.firstValue = display.textContent //store these values for future use in calculation after the 2nd set of digits are clicked
-      calculator.dataset.operator = action
+  //Handling the pure functions (arithmetic to performs) with createResultString() and showing result on display
+  display.textContent = createResultString(key, displayedNum, calculator.dataset)
 
-      calculator.dataset.previousKeyType = 'operator' //record in the dataset of calculator div that the last pressed key was an operator, to refer to when updating the display
-    }
-    if (action === "decimal") {
-      calculator.dataset.previousKeyType = "decimal"
-    }
-    if (action === 'clear') {
-      if (key.textContent === 'AC') { //reset the stored data appropriately
-        calculator.dataset.firstValue = '';
-        calculator.dataset.operator = '';
-        calculator.dataset.modifierVal = '';
-      } else {
-        key.textContent = 'AC'
-      }
-
-      calculator.dataset.previousKeyType = 'clear'
-    }
-    if (action !== 'clear') { //if any button apart from 'clear', change AC to CE
-      clearButton.textContent = 'CE';
-    }
-    if (action === 'calculate') {
-
-      calculator.dataset.modifierVal = secondValue;
-      calculator.dataset.previousKeyType = 'calculate'
-    }
-
-    //each time any key is pressed, all four operator buttons will have 'is-depressed' class removed
-    const keys = Array.from(keysDiv.children)
-    const operatorKeys = keys.filter(k => k.classList.contains('key--operator'))
-    operatorKeys
-      .forEach(k => {
-        k.classList.remove('is-depressed')
-      })
+  //Handling the impure part (changing other variables accordingly)
+  if (!action) {
+    calculator.dataset.previousKeyType = 'number' //to reset the custom dataset because now the last key pressed is not an operator key 
   }
+  if (action === "add" ||
+    action === "multiply" ||
+    action === "subtract" ||
+    action === "divide"
+  ) {
+    key.classList.add("is-depressed");
+    calculator.dataset.firstValue = display.textContent //store these values for future use in calculation after the 2nd set of digits are clicked
+    calculator.dataset.operator = action
+
+    calculator.dataset.previousKeyType = 'operator' //record in the dataset of calculator div that the last pressed key was an operator, to refer to when updating the display
+  }
+  if (action === "decimal") {
+    calculator.dataset.previousKeyType = "decimal"
+  }
+  if (action === 'clear') {
+    if (key.textContent === 'AC') { //reset the stored data appropriately
+      calculator.dataset.firstValue = '';
+      calculator.dataset.operator = '';
+      calculator.dataset.modifierVal = '';
+    } else {
+      key.textContent = 'AC'
+    }
+
+    calculator.dataset.previousKeyType = 'clear'
+  }
+  if (action !== 'clear') { //if any button apart from 'clear', change AC to CE
+    clearButton.textContent = 'CE';
+  }
+  if (action === 'calculate') {
+
+    calculator.dataset.modifierVal = secondValue;
+    calculator.dataset.previousKeyType = 'calculate'
+  }
+
+  //each time any key is pressed, all four operator buttons will have 'is-depressed' class removed
+  const keys = Array.from(keysDiv.children)
+  const operatorKeys = keys.filter(k => k.classList.contains('key--operator'))
+  operatorKeys
+    .forEach(k => {
+      k.classList.remove('is-depressed')
+    })
 })
 
 function calculate(firstNum, operation, secondNum) {
@@ -74,17 +74,6 @@ function calculate(firstNum, operation, secondNum) {
 }
 
 const createResultString = (key, displayedNum, state) => { //state param requires passing calculator.dataset, which is like holding the current state of the calculator
-  /*
-  required vars:
-
-  action
-  displayedNum
-  previousKeyType
-  keyCOntent
-  calculator.dataset.firstValue
-  calculator.dataset.operator
-  calculator.dataset.modifierVal
-   */
   const action = key.dataset.action
   const previousKeyType = state.previousKeyType
   const keyContent = key.textContent
