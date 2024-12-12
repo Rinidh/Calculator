6a3a9ec385affe7dbc68d23e8b5f6b2d7e605aed
Rinidh <rinidhdhokia@gmail.com>
2024-12-12 12:35:05 +0300

    Refactor: Split code into createResultString() (pure function) and set display.textContext always from its return value

--- index.js
+++ index.js
@@ -15,23 +15,14 @@ keysDiv.addEventListener("click", e => {
     // console.log(previousKeyType);
 
     if (!action) {
-      if (displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate') {
-        display.textContent = keyContent
-      } else {
-        display.textContent = displayedNum + keyContent //not addition but concatenation in strings
-      }
-
       calculator.dataset.previousKeyType = 'number' //to reset the custom dataset because now the last key pressed is not an operator key 
     }
-    if (action === "add" || action === "multiply" || action === "subtract" || action === "divide") {
-      const firstValue = calculator.dataset.firstValue
-      const operator = calculator.dataset.operator
-      const secondValue = display.textContent
 
-      if (firstValue && operator && previousKeyType !== 'operator' && previousKeyType !== 'calculate') {
-        display.textContent = calculate(firstValue, operator, secondValue)
-      }
-
+    if (action === "add" ||
+      action === "multiply" ||
+      action === "subtract" ||
+      action === "divide"
+    ) {
       key.classList.add("is-depressed");
       calculator.dataset.firstValue = display.textContent //store these values for future use in calculation after the 2nd set of digits are clicked
       calculator.dataset.operator = action
@@ -39,13 +30,6 @@ keysDiv.addEventListener("click", e => {
       calculator.dataset.previousKeyType = 'operator' //record in the dataset of calculator div that the last pressed key was an operator, to refer to when updating the display
     }
     if (action === "decimal") {
-      if (!displayedNum.includes('.')) {
-        display.textContent = displayedNum + '.'
-      }
-      if (previousKeyType === 'operator' || previousKeyType === 'calculate') {
-        display.textContent = '0.' //if a user directly hits the decimal after an operator, he'd mean 'zero point something'
-      }
-
       calculator.dataset.previousKeyType = "decimal"
     }
     if (action === 'clear') {
@@ -56,7 +40,6 @@ keysDiv.addEventListener("click", e => {
       } else {
         key.textContent = 'AC'
       }
-      display.textContent = 0
 
       calculator.dataset.previousKeyType = 'clear'
     }
@@ -64,18 +47,6 @@ keysDiv.addEventListener("click", e => {
       clearButton.textContent = 'CE';
     }
     if (action === 'calculate') {
-      let firstValue = calculator.dataset.firstValue
-      let secondValue = displayedNum //the 2nd set of digits on screen after the operator was clicked
-      const operator = calculator.dataset.operator
-
-      if (firstValue) {
-        if (previousKeyType === 'calculate') {
-          firstValue = displayedNum;
-          secondValue = calculator.dataset.modifierVal //the secondValue of the previous operation used again upon clicking equals button again & again
-        }
-        const result = calculate(firstValue, operator, secondValue)
-        display.textContent = result;
-      }
 
       calculator.dataset.modifierVal = secondValue;
       calculator.dataset.previousKeyType = 'calculate'
@@ -100,5 +71,59 @@ function calculate(firstNum, operation, secondNum) {
   if (operation === 'subtract') return value1 - value2
   if (operation === 'multiply') return value1 * value2
   if (operation === 'divide') return value1 / value2
+}
+
+const createResultString = (key, displayedNum, state) => { //state param requires passing calculator.dataset, which is like holding the current state of the calculator
+  /*
+  required vars:
+
+  action
+  displayedNum
+  previousKeyType
+  keyCOntent
+  calculator.dataset.firstValue
+  calculator.dataset.operator
+  calculator.dataset.modifierVal
+   */
+  const action = key.dataset.action
+  const previousKeyType = state.previousKeyType
+  const keyContent = key.textContent
+  const firstValue = state.firstValue
+  const operator = state.operator
+  const modifierVal = state.modifierVal
+
+  if (!action) {
+    return displayedNum === '0' || previousKeyType === 'operator' || previousKeyType === 'calculate'
+      ? keyContent
+      : displayedNum + keyContent //not addition but concatenation in strings
+  }
+  if (action === "decimal") {
+    if (!displayedNum.includes('.')) return displayedNum + '.'
+    if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.' //if a user directly hits the decimal after an operator, he'd mean 'zero point something'
+    return displayedNum; //if none of above match, createResultString() should not return displayedNum instead of undefined
+  }
+  if (action === "add" ||
+    action === "multiply" ||
+    action === "subtract" ||
+    action === "divide"
+  ) {
+    return (firstValue &&
+      operator &&
+      previousKeyType !== 'operator' &&
+      previousKeyType !== 'calculate'
+    ) ? calculate(firstValue, operator, display.textContent)
+      : displayedNum //in order not to return undefined if all conditions fail
+
+  }
+  if (action === "clear") {
+    return 0;
+  }
+  if (action === "calculate") {
+    return (firstValue)
+      ? (previousKeyType === 'calculate') //if firstValue is truthy then run either calculate function, or else displayedNum
+        ? calculate(displayedNum, operator, modifierVal)
+        : calculate(firstValue, operator, displayedNum)
+      : displayedNum
+  }
 }
 
